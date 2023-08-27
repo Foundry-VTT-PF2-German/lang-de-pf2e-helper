ac10058a129c4f2faa9b00e9286970d870a24684
MSAbaddon <66370966+MSAbaddon@users.noreply.github.com>
2023-08-27 19:30:15 +0200

    Add recursive sort option

--- src/util/utilities.js
+++ src/util/utilities.js
@@ -11,21 +11,25 @@ export function convertArray(sourceArray) {
 }
 
 /**
- * Recursively sort an object by key
- * @param {Object} obj
- * @returns {Object}
+ * Sort an object by key with additional sort options
+ *
+ * @param {Object} obj                              The initial object that should get sorted
+ * @param {object} [options={}]                     Additional options that configure the sort
+ * @param {boolean} [options.recursiveSort=false]   Do the sort recursive
+ * @returns {Object}                                The new sorted object
  */
-export function sortObject(obj) {
+export function sortObject(obj, { recursiveSort = false } = {}) {
     if (typeof obj !== "object" || obj === null || Array.isArray(obj)) {
         return obj;
     }
 
+    const options = { recursiveSort };
     const sortedObject = {};
 
     Object.keys(obj)
         .sort()
         .forEach((key) => {
-            sortedObject[key] = sortObject(obj[key]);
+            sortedObject[key] = recursiveSort ? sortObject(obj[key], options) : obj[key];
         });
 
     return sortedObject;
