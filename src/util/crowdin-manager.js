270ea2931226fa9d4c1c515e4ffb36e430443417
MSAbaddon <66370966+MSAbaddon@users.noreply.github.com>
2023-09-06 21:38:51 +0200

    Add crowdin manager

---
+++ src/util/crowdin-manager.js
@@ -0,0 +1,66 @@
+import crowdin from "@crowdin/crowdin-api-client";
+
+/**
+ * Get the project labels for a specified Crowdin project
+ *
+ * @param {string} projectId    Crowdin project id
+ * @param {string} token        Crowdin API access token
+ * @returns {Array<Object>}     Object array containing the labels
+ */
+export function getLabels(projectId, token) {
+    const { labelsApi } = new crowdin.default({ token: token });
+
+    return labelsApi.listLabels(projectId, { limit: 500 }).then((labels) => {
+        return labels.data;
+    });
+}
+
+/**
+ * Get the source files for a specified crowdin project
+ *
+ * @param {string} projectId    Crowdin project id
+ * @param {string} token        Crowdin API access token
+ * @returns {Array<Object>}     Object array containing the source files
+ */
+export function getSourceFiles(projectId, token) {
+    const { sourceFilesApi } = new crowdin.default({ token: token });
+
+    return sourceFilesApi.listProjectFiles(projectId, { limit: 500 }).then(async (files) => {
+        return files.data;
+    });
+}
+
+/**
+ * Get the source strings for a source file within a specified crowdin project
+ *
+ * @param {string} projectId    Crowdin project id
+ * @param {string} token        Crowdin API access token
+ * @param {string} sourceFile   Crowdin source file id
+ * @returns {Array<Object>}     Object array containing the source strings
+ */
+export function getSourceStrings(projectId, token, sourceFile) {
+    const { sourceStringsApi } = new crowdin.default({ token: token });
+    return new Promise(async (resolve) => {
+        let sourceStrings = [];
+        let offsetCounter = 0;
+        let limit = 500;
+        let arrayHasData = true;
+        do {
+            await sourceStringsApi
+                .listProjectStrings(projectId, {
+                    fileId: sourceFile,
+                    limit: limit,
+                    offset: offsetCounter * limit,
+                })
+                .then((sourceStringData) => {
+                    if (sourceStringData.data.length > 0) {
+                        sourceStrings = sourceStrings.concat(sourceStringData.data);
+                    } else {
+                        arrayHasData = false;
+                    }
+                });
+            offsetCounter = offsetCounter + 1;
+        } while (arrayHasData);
+        resolve(sourceStrings);
+    });
+}
